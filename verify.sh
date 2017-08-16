856b85b6350cca8b284299eec9d7c26739aae2fc
Akhil Mohan <akhilerm@gmail.com>
2017-08-16 10:10:20 +0530

    userid added to verify.sh

--- verify.sh
+++ verify.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 set -e
-#arguments filename, question_name, working directory
+#arguments filename, question_name, working directory, user_id
 filename=$1
 question=$2
 workdir=$3
+userid=$4
 
 #preparing folder for code testing. changing test case file name and creating python module by __init__.py
 sed -i s/question_test_file/$question/g $workdir/answers/driver.py
 touch $workdir/users/1/$question/__init__.py
 
-result=$(docker run --rm -v $workdir/users/1/$question:/tmp/user:ro -v $workdir/answers:/tmp/answer:ro python:df)
+result=$(docker run --rm -v $workdir/users/$userid/$question:/tmp/user:ro -v $workdir/answers:/tmp/answer:ro python:df)
 error=$(echo $result | grep -o "Traceback")
 
 #reverting back the changes made before execution
