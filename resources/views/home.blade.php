3e3905d48374389a9a4dc0887220253edd6a7564
swarajKR <swarajkrishnan@outlook.com>
2017-08-20 19:29:36 +0530

    ajax added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.appTwo')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Castle</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <div id="term"></div>
+                    </body>
                 </div>
             </div>
         </div>
