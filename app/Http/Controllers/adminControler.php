01d5f519529ac338888ca510f65f76f1b870d094
Swaraj Krishnan <swarajkrishnan@outlook.com>
2017-08-23 11:53:30 +0530

    " changed cd and help"
    help works only with args

--- app/Http/Controllers/adminControler.php
+++ app/Http/Controllers/adminControler.php
@@ -2,75 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
 use Request;
 
 class adminControler extends Controller
 {
     //
-    private $CMD_COLOR;
-    private $DIR_COLOR;
-    private $WORK_DIR;
-    public function __construct()
-    {
-        //$this->middleware('admin');
-        $this->CMD_COLOR = '#FFA500';
-        $this->DIR_COLOR = '#0000FF';
-        $this->WORK_DIR = '/home/akhil/Work/HTML/www/term/storage/app/public/';
-    }
 
     public function shell()
     {
         if (Request::ajax()){
+
             $req = Request::all();
+            $settings = ['CMD_COLOR' =>  '#FFA500', 'DIR_COLOR' => '#0000FF', 'WORK_DIR' => 'public/users/'];
 
             //check if func exist and call
             if (method_exists($this, $req['method'])){
-                return call_user_func( array($this, $req['method']),$req);
+                return call_user_func_array( array($this, $req['method']),[$req['args'], $settings]);
             }
 
             // The response
-            return response()->json($req);
+            return response()->json(['args'=>$req['method']]);
         }
     }
 
-    public function cd($args){
-        if ($this->check("cd", $args)) {
-            if ($args[0] == ".."){
-                session(['pwd'=>'~']);
-            }
-            else if ($args[0] != "."){
-                $dir = $this->WORK_DIR.'/users/'.Auth::user()['id'].'/';
-                if (session('pwd')!='~') {
-                    $dir = $dir.explode("/", session('pwd'))[1].'/';
-                }
-                $files = preg_grep('/^([^.])/', scandir($dir));
-                $folder = "NIL";
-                foreach ($files as $file) {
-                    if (is_dir($dir.$file)) {
-                        $folder = $file;
-                    }
-                }
-                if ($folder!="NIL" && $folder ==$args[0]) {
-                    session(['pwd'=>"~/".$folder]);
-                }
-                else{
-                    $result[0] = "\ncd: ".$args[0].": Not a directory\n";
-                    $result[1] = "false";
-                    return $result;
+
+    /**
+     * CHANGE DIR: if arg is .. switch to home else switch to folder if exists
+     *
+     * @param $args
+     * @param $settings
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function cd($args, $settings)
+    {
+
+        if ( $args[0] === '..' || $args[0] === '../' || $args[0] === '~'){
+
+            Session::put('pwd', '~');
+            $msg = Auth::user()['name'].'@Castle: '.session('pwd').' $ ';
+            $sts  =true;
+
+        }
+        elseif ( $args[0] !==  '.'){
+
+            //ADDRESS TO  Users home directory
+            $user_dir = $settings['WORK_DIR'].Auth::id();
+
+            //Check if the folder exists if in home
+            if(Session::get('pwd') === '~'){
+
+                $user_dir = "$user_dir/$args[0]";
+                if(Storage::has("$user_dir/")){
+
+                    Session::put('pwd', $args[0]);
+                    $msg = Auth::user()['name'].'@Castle: '.session('pwd').' $ ';
+                    $sts  =true;
+                    return response()->json( ['STS'=> $sts, 'MSG' => $msg] );
+
                 }
 
             }
 
-            $MSG=Auth::user()['name'].'@Castle:'.session('pwd').'/$ ';
-            $STS=true;
+            //No Directory
+            $msg="cd: $args[0]: No such directory";
+            $sts=false;
+
+        } else{
+
+            //Keeping it in the same directory
+            $msg = Auth::user()['name'].'@Castle: '.session('pwd').' $ ';
+            $sts  =true;
+
         }
-        else {
-            $MSG='cd: '.args[0].': No such directory';
-            $STS=false;
-        }
-        $result['STS']=$STS;
-        $result['MSG']=$MSG;
-        return response()->json($result);
+
+        return response()->json( ['STS'=> $sts, 'MSG' => $msg] );
     }
 
     public function cat($args) {
@@ -93,23 +102,20 @@ class adminControler extends Controller
         }
     }
 
-    public function help($args)
+    /**
+     * Return a String as response
+     *
+     * @param $args
+     * @param $settings
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function help($args, $settings)
     {
-        if ($this->check("help", $args)) {
-            $result['MSG'] = "\nUse the following shell commands:
-[[;" . $this->CMD_COLOR . ";]cd]     - change directory [dir_name]
-[[;" . $this->CMD_COLOR . ";]cat]    - print file [file_name]
-[[;" . $this->CMD_COLOR . ";]clear]  - clear the terminal
-[[;" . $this->CMD_COLOR . ";]edit]   - open file in editor [file_name]
-[[;" . $this->CMD_COLOR . ";]ls]     - list directory contents [dir_name]
-[[;" . $this->CMD_COLOR . ";]logout] - logout from Castle
-[[;" . $this->CMD_COLOR . ";]request]- request a new challenge
-[[;" . $this->CMD_COLOR . ";]status] - print progress
-[[;" . $this->CMD_COLOR . ";]submit] - submit final solution for assessment [file_name]
-[[;" . $this->CMD_COLOR . ";]verify] - runs tests on solution file [file_name]\n";
-            $result['STS'] = true;
-            return response()->json($result);
-        }
+
+        $msg  = "\nUse the following shell commands: \n[[;" . $settings['CMD_COLOR']. ";]cd]     - change directory [dir_name] \n[[;" . $settings['CMD_COLOR']. ";]cat]    - print file [file_name] \n[[;" . $settings['CMD_COLOR']. ";]clear]  - clear the terminal \n[[;" . $settings['CMD_COLOR']. ";]edit]   - open file in editor [file_name] \n[[;" . $settings['CMD_COLOR']. ";]ls]     - list directory contents [dir_name] \n[[;" . $settings['CMD_COLOR']. ";]logout] - logout from Castle \n[[;" . $settings['CMD_COLOR']. ";]request]- request a new challenge \n[[;" . $settings['CMD_COLOR']. ";]status] - print progress \n[[;" . $settings['CMD_COLOR']. ";]submit] - submit final solution for assessment [file_name] \n[[;" . $settings['CMD_COLOR']. ";]verify] - runs tests on solution file [file_name]\n";
+        return response()->json([ 'STS' => true, 'MSG' => $msg]);
+
     }
 
     public function ls($args) {
