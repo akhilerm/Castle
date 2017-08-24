32d3cd54af52f849d3af4a0d9877a3c9e88c2777
Akhil Mohan <akhilerm@gmail.com>
2017-08-24 13:31:41 +0530

    Models declared

---
+++ app/Models/level.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class level extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'levels';
+}
