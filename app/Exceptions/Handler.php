7e52be7900c7d30f5af2db9d532586444d59a29d
swarajKR <swarajkrishnan@outlook.com>
2017-08-30 11:25:59 +0530

    changes in master

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,6 +45,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof NotFoundHttpException){
+            return response()->view('error');
+        }
         return parent::render($request, $exception);
     }
 
