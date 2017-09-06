4037678bcb75cf7e0dc813f55634099c0c6a2884
Akhil Mohan <akhilerm@gmail.com>
2017-09-06 11:22:54 +0530

    adding languages

--- storage/app/public/drivers/driver.sh
+++ storage/app/public/drivers/driver.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
-result=$(timeout 20 python /tmp/answer/driver.py 2>&1)
-echo $result
+
+
+result=$(timeout 20 python solution.py  2>&1)
+echo "HI" $result "BY"
--- storage/app/public/drivers/verify.sh
+++ storage/app/public/drivers/verify.sh
@@ -7,23 +7,39 @@ question=$2
 workdir=$3
 userid=$4
 
-#preparing folder for code testing. changing test case file name and creating python module by __init__.py
-sed -i s/question_test_file/$question/g $workdir/answers/driver.py
-touch $workdir/users/$userid/$question/__init__.py
-
-result=$(docker run --rm -v $workdir/users/$userid/$question:/tmp/user:ro -v $workdir/answers:/tmp/answer:ro python:df)
-
-#reverting back the changes made before execution
-rm $workdir/users/$userid/$question/__init__.py
-sed -i s/$question/question_test_file/g $workdir/answers/driver.py
-
-if [ -z "$result" ];
-    then echo "FAIL"
-    echo "Resource limit reached"
-elif [[ $result == *"Traceback"* ]];
-    then echo "FAIL"
-    echo "Error in "$filename
-else
-    echo "TRUE"
-    echo $result
+#can add support for more languages here
+if [[ $filename == *".java"* ]];
+	then lang="JAVA"
+elif [[ $filename == *".py"* ]];
+	then lang="PYTHON"
 fi
+
+case $lang in
+	JAVA )
+	echo "JAVA"
+	;;
+	PYTHON )
+	echo "PYTHON"
+	;;
+esac
+
+#preparing folder for code testing. changing test case file name and creating python module by __init__.py
+# sed -i s/question_test_file/$question/g $workdir/answers/driver.py
+# touch $workdir/users/$userid/$question/__init__.py
+
+# result=$(docker run --rm -v $workdir/users/$userid/$question:/tmp/user:ro -v $workdir/answers:/tmp/answer:ro python:df)
+
+# #reverting back the changes made before execution
+# rm $workdir/users/$userid/$question/__init__.py
+# sed -i s/$question/question_test_file/g $workdir/answers/driver.py
+
+# if [ -z "$result" ];
+#     then echo "FAIL"
+#     echo "Resource limit reached"
+# elif [[ $result == *"Traceback"* ]];
+#     then echo "FAIL"
+#     echo "Error in "$filename
+# else
+#     echo "TRUE"
+#     echo $result
+# fi
