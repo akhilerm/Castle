0b450148173e80f78aea20729af3a4192aeeb5ab
Akhil Mohan <akhilerm@gmail.com>
2017-09-07 17:28:21 +0530

    driver files moved to container

--- storage/app/public/drivers/driver.sh
+++ storage/app/public/drivers/driver.sh
@@ -6,10 +6,19 @@ question=$2
 cd /tmp/driver
 
 case $lang in
+
     JAVA )
+    #copy solution and answer file
+    cp ../user/solution.java Answer.java
+    cp ../answer/$question answer
+
+    rm answer
+    rm Answer.java
+    rm Answer.class
     ;;
 
     PYTHON )
+    #copy solution file and answer file
     cp ../user/solution.py solution.py
     cp ../answer/$question answer
     result=$(timeout 20 python driver.py  2>&1)
@@ -20,6 +29,7 @@ case $lang in
     else
         echo $result
     fi
+    #removing the copied files and output files
     rm solution.py
     rm solution.pyc
     rm answer
