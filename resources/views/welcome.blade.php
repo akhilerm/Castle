da4dc541510221274fb654250b5346113236631f
Akhil Mohan <akhilerm@gmail.com>
2017-09-15 15:25:23 +0530

    removed redirection

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Castle</title>
-        <script type="text/javascript">
-            <!--
-            if (screen.width <= 699) {
-            document.location = "http://www.google.com";
-            }
-            //-->
-            </script>
+        
         <!-- Fonts -->
        <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
 
